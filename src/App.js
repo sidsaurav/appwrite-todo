f55fe35129ccea6884e04cb7c875dfbfe97f2052
Siddharth Saurav <sid.saurav11@gmail.com>
2023-05-27 23:20:27 +0530

    add login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react"
 import Todo from "./components/Todo"
+import { Route, Routes } from "react-router-dom"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
 
 const App = () => {
-  return <Todo />
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Todo />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
