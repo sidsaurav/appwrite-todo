c4fc3a3eec6268603ac6e5f814150a67e8dc4c1d
Siddharth Saurav <sid.saurav11@gmail.com>
2023-05-28 20:34:22 +0530

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react"
 import Todo from "./components/Todo"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Link, Navigate } from "react-router-dom"
+import api from "./server/api"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
 
 const App = () => {
   return (
     <>
+      <nav className='flex justify-center text-xl mt-3'>
+        <ul className='flex'>
+          <li className='mx-6'>
+            <Link to='/login'>Login</Link>
+          </li>
+
+          <li className=''>
+            <Link to='/signup'>Signup</Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
         <Route path='/' element={<Todo />} />
         <Route path='/login' element={<Login />} />
