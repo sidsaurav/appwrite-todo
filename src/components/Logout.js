27102b61f018aa749f186215e0d33791de7be399
Siddharth Saurav <sid.saurav11@gmail.com>
2023-06-28 11:47:51 +0530

    new commit

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react"
-import api from "../server/api"
-import { useAuth } from "../hooks/useAuth"
-import { useNavigate } from "react-router-dom"
-import Spinner from "./Spinner"
+import React, { useEffect } from 'react'
+import api from '../server/api'
+import { useAuth } from '../hooks/useAuth'
+import { useNavigate } from 'react-router-dom'
+import Spinner from './Spinner'
 const Logout = () => {
   const navigate = useNavigate()
   const { setUser } = useAuth()
@@ -11,7 +11,6 @@ const Logout = () => {
       try {
         const res = await api.deleteCurrentSession()
         setUser(null)
-        navigate("/")
       } catch (err) {
         console.log(err.message)
       }
